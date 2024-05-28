41f042e5a40551039cb372c006448581a05cf777
Sahar Mughal <saharmughal2022@gmail.com>
2024-05-29 02:31:23 +0500

    Add files via upload

---
+++ Class 5 Conditional Statments/main.ts
@@ -0,0 +1,25 @@
+// coditional statements (if, else if, else)
+
+let fruit_price = 120;
+
+if (fruit_price==250){
+    console.log("this price is so high");
+    console.log(150+100);
+} // statement match with above statement.
+
+if (fruit_price==100){
+    console.log("this is reasonable price");
+} // this statment did not match with above so is did not run in terminal.
+else if (fruit_price==120){
+    console.log('price is acceptable');
+}
+else {
+    console.log('this price did not match with actual');
+} // by using else wrong statement run.
+
+//Symbole Data Type
+
+
+ let class1 = Symbol('1');
+let class2 = Symbol('2');
+ console.log(class1 == class2);
---
+++ Class 5 Conditional Statments/readme.md
@@ -0,0 +1,7 @@
+create main ts file, 
+create main js file by using cmd 'tsc main.ts' or 'tsc file name.ts
+creat json file by using cmd 'tsc -init'
+and pacakage json use cmd 'npm init -y'
+for node modules use cmd 'npm i @types/node' and then creat .gitignore file
+Conditional statements: if, else if, else
+symbole usage
