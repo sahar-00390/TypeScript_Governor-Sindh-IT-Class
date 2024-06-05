e4e10a01632c0c8a1c340af99339961e2aef26fa
Sahar Mughal <saharmughal2022@gmail.com>
2024-06-06 01:10:54 +0500

    Add files via upload

---
+++ Class 7 Increment Decrement/main.js
@@ -0,0 +1,16 @@
+"use strict";
+// Increment & Decrement
+// Increment
+let x = 60;
+x = x + 1;
+++x;
+x++;
+console.log(x++);
+console.log(++x);
+console.log(x);
+//  decrement
+let y = 50;
+--y;
+console.log(y);
+--y;
+console.log(y);
